8a8cbc77da69cfbb2d059f03a9d04108c4dad4c4
Kartik47 <kumthekarkartik@gmail.com>
2025-04-15 22:11:18 +0530

    MiddleWare

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json())
 // app.use(cors())
 
-const port = 7002;
+const port = 3000;
 
 app.get('/api', (req, res) => {
     res.status(202).send({ message: 'Server running successfully', greet: "Hello user", success: true });
@@ -36,9 +36,59 @@ app.get('/contact', (req, res) => {
     res.send("This is Contact");
 });
 
+
+const products  = [{id:1, name:"iPhone15pro", category:"Electronics", price:120000, instock:true},
+{id:2, name:"iPhone16pro", category:"Electronics", price:120200, instock:true}
+]
+
+app.get("/getAllProducts",(req,res)=>{
+res.status(200).send({products:products, success:true})
+})
+
+app.get("/getProductsByID/:ID",(req,res)=>{
+    const id = req.params.ID;
+    const prod = products.filter(p=> p.id == id)
+    res.status(200).send({products:prod, success:true})
+})
+
+app.post("/addProducts",(req,res)=>{
+    console.log("----", req.body, "-----")
+    const newProduct = {id:Date.now(),
+        name:req.body.name,
+        category:req.body.category,
+        price:req.body.price,
+        instock:true
+    }
+
+    products.push(newProduct);
+    res.send({message: "product added successfully", success:true})
+})
+
+app.delete('/deleteProduct/:ID', (req,res)=>{
+    const ID = req.params.ID
+    index = products.findIndex(p=> p.id == ID)
+    if(index == -1){
+        res.status(200).send({message:'products Not Found',success:false})
+    }
+    products.splice(index,1)
+    res.status(200).send({message:'products Deleted', success:true})
+})
+
+
+app.put("/updateProduct/:ID",(req,res)=>{
+    const ID = req.params.ID
+    index = products.findIndex(p=> p.id == ID)
+    if (index == -1){
+        res.status(200).send({message:'product not found', success:false})
+    }
+    products[index].price = req.body.price
+    res.status(200).send({message:'updated product', success:true})
+})
+
+
+
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`.rainbow);
 });
-
 
 // http://localhost:7002/api
